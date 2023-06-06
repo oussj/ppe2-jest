8f94a05a82e33000d101d5a5fbc1f782239204fe
color.branch <philippe.malezieux@afnor.org>
2023-06-06 20:09:08 +0200

    login valider

--- backend/src/Controller/ApiMessageController.php
+++ backend/src/Controller/ApiMessageController.php
@@ -3,36 +3,52 @@ namespace App\Controller;
 use App\Entity\Messages;
 use App\Entity\User;
 use App\Repository\MessagesRepository;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ApiMessageController extends AbstractController
 {
     #[Route('/apimessage', name: 'app_api_message', methods:['POST'])]
     public function index(Request $request, MessagesRepository $messagesRepository): Response
     {
-        $data = json_decode($request->getContent(), true
-    );
+        $data = json_decode(
+            $request->getContent(),
+            true
+        );
         $content = $data['content'];
         $senderId = $data['sender_id'];
         $receiverId = $data['receiver_id'];
-        
+
         $message = new Messages();
         $message->setContent($content);
-        
+
         // Retrieve sender and receiver entities from their IDs
         $sender = $messagesRepository->find($senderId);
-        $receiver =$messagesRepository->find($receiverId);
-        
+        $receiver = $messagesRepository->find($receiverId);
+
         $message->setSender($sender);
         $message->setReceiver($receiver);
-        
+
         // Persist the entity to the database
         $messagesRepository->save($message, true);
         dump($message);
         return new JsonResponse([$content]);
+    }
+
+    #[Route('/apiuser', name: 'app_user_api', methods: ['POST'])]
+    public function callUser (UserRepository $userRepository, SerializerInterface $serializerInterface): Response
+    {
+        $users = $userRepository->findAll();
+        //$userArray = array_map(fn(User $user) => $user->toArray(), $user_id);
+        $jsonContent = $serializerInterface->serialize($users, 'json');
+        $response = new JsonResponse($jsonContent, 200, [
+            'Content-Type' => 'application/json'
+        ]);
+        return $response;
     }
 }
---
+++ frontend/components/User.tsx
@@ -0,0 +1,49 @@
+// component/User.tsx
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+export type User = {
+  id: string;
+  email: string;
+};
+
+type UserProps = {
+  onUserSelected: (User: any) => void;
+};
+
+export const UserSelect = ({ onUserSelected }: UserProps) => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/apiuser`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      const parsedResponse = JSON.parse(response.data); // Conversion de la réponse en objet JS
+      setUsers(parsedResponse);
+    };
+    fetchUsers();
+  }, []);
+
+  const handleUserChange = (event: any) => {
+    const selectedUserId = event.target.value; // Récupérer l'ID de l'utilisateur sélectionné
+    onUserSelected(selectedUserId);
+  };
+
+  return (
+    <select onChange={handleUserChange}>
+      <option>Select user</option>
+      {users.map((user) => (
+        <option key={user.id} value={user.id}>
+          {user.email}
+        </option>
+      ))}
+    </select>
+  );
+};
